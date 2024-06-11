6249a6e26cff885d7429e7625cf415e848c4b456
LimJiNa <jn4624@gmail.com>
2024-06-12 00:17:20 +0900

    순수 JPA 기반 레포지토리 구현 및 테스트

--- src/main/java/study/repository/MemberJpaRepository.java
+++ src/main/java/study/repository/MemberJpaRepository.java
@@ -5,6 +5,9 @@ import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 import study.entity.Member;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public class MemberJpaRepository {
     @PersistenceContext
@@ -13,6 +16,25 @@ public class MemberJpaRepository {
     public Member save(Member member) {
         entityManager.persist(member);
         return member;
+    }
+
+    public void delete(Member member) {
+        entityManager.remove(member);
+    }
+
+    public List<Member> findAll() {
+        return entityManager.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public Optional<Member> findById(Long id) {
+        Member member = entityManager.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    public long count() {
+        return entityManager.createQuery("select count(m) from Member m", Long.class)
+                .getSingleResult();
     }
 
     public Member find(Long id) {
