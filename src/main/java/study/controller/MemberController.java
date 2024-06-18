0be6502cebfad72885845eb8274dbcc964563246
LimJiNa <jn4624@gmail.com>
2024-06-18 23:12:28 +0900

    Web 확장 - 도메인 클래스 컨버터 사용 후

--- src/main/java/study/controller/MemberController.java
+++ src/main/java/study/controller/MemberController.java
@@ -19,6 +19,17 @@ public class MemberController {
         return member.getUsername();
     }
 
+    /**
+     * 주의:
+     * 도메인 클래스 컨버터로 엔티티를 파라미터로 받으면,
+     * 이 엔티티는 단순 조회용으로만 사용해야 한다.
+     * (트랜잭션이 없는 범위에서 엔티티를 조회했으므로, 엔티티를 변경해도 데이터베이스에 반영되지 않는다.)
+     */
+    @GetMapping("/members/{id}/converter")
+    public String findMemberConverter(@PathVariable("id") Member member) {
+        return member.getUsername();
+    }
+
     @PostConstruct
     public void init() {
         memberRepository.save(new Member("userA"));
