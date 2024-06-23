d02f9402a71c9c29e1d4c9cfb012f3bc3df17b0c
LimJiNa <jn4624@gmail.com>
2024-06-23 13:17:01 +0900

    새로운 엔티티 구별하는 방법 - @GeneratedValue 미사용시 merge 동작 이슈 해결 방법

--- src/main/java/study/entity/Item.java
+++ src/main/java/study/entity/Item.java
@@ -1,12 +1,19 @@
 package study.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.Id;
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.domain.Persistable;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
-public class Item {
+@EntityListeners(AuditingEntityListener.class)
+public class Item implements Persistable<String> {
 //    @Id
 //    @GeneratedValue
 //    private Long id;
@@ -14,10 +21,18 @@ public class Item {
     @Id
     private String id;
 
+    @CreatedDate
+    private LocalDateTime createdDate;
+
     protected Item() {
     }
 
     public Item(String id) {
         this.id = id;
+    }
+
+    @Override
+    public boolean isNew() {
+        return createdDate == null;
     }
 }
